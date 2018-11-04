c71ecb01285f8714186a837c40379f9bc113fc24
Duverney Hernandez Mora <du.hernandez@udla.edu.co>
2018-11-04 03:21:49 -0500

    Inicio del proyecto - en honor a todos, las palabras son tan cortas, las lagrimas gruesas y la gratitud por todo mayor

---
+++ src/laboratoryplaygame/LaboratoryPlayGame.java
@@ -0,0 +1,27 @@
+/*
+ * El objetivo principal del desarrollo del juego es hallar aplicabilidad a
+ * múltiples temas de las ciencias matemáticas aplicadas a la ingeniería, entre
+ * ellos, el trabajo con funciones lineales (fundamentales en programación
+ * lineal y todas sus variantes (entera, binaria y mixta)), trigonometría, etc.,
+ * y demás temas abordados en la carrera Ingeniería de Sistemas de la
+ * Universidad de la Amazonia y que constituye la materialización de mis
+ * recuerdos de todos los docentes que entregaron lo mejor de si mismos no solo
+ * desde la parte teórico-práctico sino que supieron llegar a la mente y al
+ * corazón
+ */
+package laboratoryplaygame;
+
+/**
+ *
+ * @author Duverney Hernandez Mora
+ */
+public class LaboratoryPlayGame {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+    }
+    
+}
